a9ef5e996cf1f614465b5f6f4450ccd0c5f5c552
gaeguriBanchan <rlawowls93@gamil.com>
2023-04-11 12:37:08 +0900

    contact 작업

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ window.onload = function () {
   AOS.init();
 
   // 모바일 메뉴 관련
-  const navMb = document.querySelector('.nav-mb');
-  const mbWrap = document.querySelector('.mb-wrap');
-  navMb.addEventListener('click', function () {
+  const navMb = document.querySelector(".nav-mb");
+  const mbWrap = document.querySelector(".mb-wrap");
+  navMb.addEventListener("click", function () {
     // 인라인 방식
     // if (mbWrap.style.display === "block") {
     //   mbWrap.style.display = "none";
@@ -14,33 +14,33 @@ window.onload = function () {
     // }
 
     // 클래스 추가 방식
-    const state = mbWrap.classList.contains('active');
+    const state = mbWrap.classList.contains("active");
     if (state) {
-      navMb.classList.remove('active');
-      mbWrap.classList.remove('active');
+      navMb.classList.remove("active");
+      mbWrap.classList.remove("active");
     } else {
-      navMb.classList.add('active');
-      mbWrap.classList.add('active');
+      navMb.classList.add("active");
+      mbWrap.classList.add("active");
     }
   });
 
   // 비주얼 슬라이드
   // 동적 즉, 서버에서 슬라이드를 생성하는 경우
   const slideTotal = document.querySelectorAll(
-    '.swVisual .swiper-slide'
+    ".swVisual .swiper-slide"
   ).length;
-  const slideControlUl = document.querySelector('.sw-visual-control');
-  let html = '';
+  const slideControlUl = document.querySelector(".sw-visual-control");
+  let html = "";
   for (let i = 0; i < slideTotal; i++) {
     let tempI = i + 1;
     if (i < 9) {
-      tempI = '0' + (i + 1);
+      tempI = "0" + (i + 1);
     }
     html += `<li>${tempI}</li>`;
   }
   slideControlUl.innerHTML = html;
 
-  const swVisual = new Swiper('.swVisual', {
+  const swVisual = new Swiper(".swVisual", {
     loop: true,
     speed: 800,
     autoplay: {
@@ -48,25 +48,25 @@ window.onload = function () {
       disableOnInteraction: false,
     },
     navigation: {
-      nextEl: '.sw-visual-next',
-      prevEl: '.sw-visual-prev',
+      nextEl: ".sw-visual-next",
+      prevEl: ".sw-visual-prev",
     },
   });
 
   // swVisual.on("slideChange", function () {
   //   console.log("slide changed");
   // });
-  swVisual.on('slideChangeTransitionStart', function () {
+  swVisual.on("slideChangeTransitionStart", function () {
     // console.log("slide changed", this.realIndex);
     changeSwVisual(this.realIndex);
   });
 
   // 비주얼 슬라이드 제어 목록
-  let swVisualControlLi = document.querySelectorAll('.sw-visual-control li');
+  let swVisualControlLi = document.querySelectorAll(".sw-visual-control li");
   // console.log(swVisualControlLi);
   swVisualControlLi.forEach((item, index) => {
     // console.log(item);
-    item.addEventListener('click', function () {
+    item.addEventListener("click", function () {
       // 내가 만드는 함수
       changeSwVisual(index);
     });
@@ -76,10 +76,10 @@ window.onload = function () {
     // css 처리 (포커스 클래스 추가 및 제거)
     // 1. active 클래스 모두 제거
     swVisualControlLi.forEach((item, index) => {
-      item.classList.remove('active');
+      item.classList.remove("active");
     });
     // 2. 클릭된 번호를 이용해서 클래스 추가하기
-    swVisualControlLi[_idx].classList.add('active');
+    swVisualControlLi[_idx].classList.add("active");
     // 3. 클릭된 번호를 이용해서 Swiper 를 이동
     // * 주의 사항 (loop 를 옵션으로 준 경우는 아래 구문 금지)
     // swVisual.slideTo(_index);
@@ -92,7 +92,59 @@ window.onload = function () {
   }
 
   // brandSlide
-  const swBrand = new Swiper('.swBrand');
+  let swBrandBts = document.querySelectorAll(".swBrandMenu > li");
+  const swBrand = new Swiper(".swBrand", {
+    pagination: {
+      el: ".swBrand-pg ",
+      clickable: true,
+    },
+    effect: "fade",
+    speed: 1000,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  });
+
+  swBrand.on("slideChangeTransitionStart", function () {
+    // console.log("slide changed", this.realIndex);
+    changeSwBrandFocus(this.realIndex);
+  });
+
+  swBrandBts.forEach((item, index) => {
+    item.addEventListener("click", function (e) {
+      // href 막기
+      e.preventDefault();
+      // 인덱스 번호를 넘겨서 슬라이드를 이동한다.
+      changeSwBrand(index);
+    });
+  });
+
+  function changeSwBrand(index) {
+    // 슬라이드 이동
+    swBrand.slideTo(index);
+    changeSwBrandFocus(index);
+  }
+
+  // active 클래스 이동
+  function changeSwBrandFocus(index) {
+    // li 태그에서 active 클래스 모두 지우기
+    swBrandBts.forEach((item) => {
+      item.classList.remove("active");
+    });
+    // 하나만 포커스(active) 클래스 적용
+    swBrandBts[index].classList.add("active");
+  }
+
+  // 자동 플레이 막기/재실행
+  const swBrandWrap = document.querySelector(".swBrandWrap");
+
+  swBrandWrap.addEventListener("mouseenter", function () {
+    swBrand.autoplay.stop();
+  });
+  swBrandWrap.addEventListener("mouseleave", function () {
+    swBrand.autoplay.start();
+  });
 
   // Visual Swiper 스케일 효과
   // 참조 https://bkstudio.tistory.com/6
@@ -103,12 +155,12 @@ window.onload = function () {
   let scTop = window.scrollY || window.pageYOffset;
 
   // 변화를 줄 대상
-  let swVisualWrap = document.querySelector('.swVisual-wrap');
+  let swVisualWrap = document.querySelector(".swVisual-wrap");
 
   /* effectText 인터렉션 */
   let stX = 50; // 50% 를 기준으로 한다. (translate)
-  let effect_01 = document.querySelector('.effect-01');
-  let effect_02 = document.querySelector('.effect-02');
+  let effect_01 = document.querySelector(".effect-01");
+  let effect_02 = document.querySelector(".effect-02");
 
   // 변화를 적용할 함수
   function swVisualMove() {
@@ -134,9 +186,11 @@ window.onload = function () {
 
   function effectText() {
     let value02 = stX * 2;
+    //https://developer.mozilla.org/ko/docs/Web/API/Element/getBoundingClientRect
     let rect =
-      document.querySelector('.effect').getBoundingClientRect().top +
+      document.querySelector(".effect").getBoundingClientRect().top +
       window.scrollY;
+    // windHeight: 웹브라우저 내용(상단 웹브라우저 메뉴 제거한 높이)
     let offset = rect - winHeight;
 
     let calvalue = scTop - offset;
@@ -145,22 +199,24 @@ window.onload = function () {
     if (window.innerWidth <= 540) {
       ratio = ratio * 0.5;
     }
-    let cssTxt = 'translate(' + ratio + '%,0px)';
+    let cssTxt = "translate(" + ratio + "%,0px)";
     effect_01.style.transform = cssTxt;
+    // console.log(cssTxt);
 
     // ratio = -ratio;
-    let cssTxt2 = 'translate(' + -1 * ratio + '%,0px)';
+    let cssTxt2 = "translate(" + -1 * ratio + "%,0px)";
     effect_02.style.transform = cssTxt2;
+    // console.log(cssTxt2);
   }
 
   // 기준값 갱신
-  window.addEventListener('scroll', function () {
+  window.addEventListener("scroll", function () {
     winHeight = window.innerHeight;
     scTop = window.scrollY || window.pageYOffset;
     swVisualMove();
     effectText();
   });
-  window.addEventListener('resize', function () {
+  window.addEventListener("resize", function () {
     winHeight = window.innerHeight;
     scTop = window.scrollY || window.pageYOffset;
     swVisualMove();
